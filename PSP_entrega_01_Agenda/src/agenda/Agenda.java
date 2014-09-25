40e91c486fb60ac9d05957828d6b9621ef76c226
tatianagprieto@gmail.com <tatianagprieto@gmail.com@56ce70eb-5c0e-736a-ac54-b164219d9608>
2014-09-25 17:59:36 +0000

    Primer commit, probando Subversion

---
+++ PSP_entrega_01_Agenda/src/agenda/Agenda.java
@@ -0,0 +1,42 @@
+// Crear una agenda en la que se pueda añadir, eliminar y buscar un contacto. 
+//Los datos de la agenda se guardarán en un fichero que se deberá poder guardar y cargar en la memoria. 
+//Los datos se guardarán en memoria y cuando se cargue una agenda nueva, éstos serán sobreescritos 
+//completamente a fin de evitar contactos duplicados. Realizar la interfaz gráfica. 
+//Entregar también el diagrama de clases correspondiente. 
+//El trabajo se puede realizar en parejas y tiene dos semanas de plazo de entrega
+//
+//Recomendado repasar:
+//UML
+//Diagrama de clases
+//Conceptos de entrada/salida
+//inputStream/outputStream [PrintStream(out)]
+//Colecciones de java
+//listas (sort/hashmaps)
+//Interfaz gráfica
+//Concepto de frame
+//
+//
+//Contacto
+//String nombre
+//int numero
+//
+//anadirContacto(): void
+//borrarContacto(): void
+//modificarContacto(): void
+//buscarContacto(): void
+//
+//Agenda
+//List <Contacto> agenda
+//guardarAgenda(): void
+//leerAgenda(): void
+//cargarAgenda():void
+//
+
+
+
+package agenda;
+
+public class Agenda {
+
+
+}//FIN CLASE AGENDA
