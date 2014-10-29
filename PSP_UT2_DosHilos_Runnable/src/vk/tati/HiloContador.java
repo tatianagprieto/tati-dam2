ff4f93d184344f69d539ec2b310c6aa153882a19
tatianagprieto@gmail.com <tatianagprieto@gmail.com@56ce70eb-5c0e-736a-ac54-b164219d9608>
2014-10-29 22:34:29 +0000

    Igual que la versión anterior, pero comentada. La próxima versión, la definitiva.

--- PSP_UT2_DosHilos_Runnable/src/vk/tati/HiloContador.java
+++ PSP_UT2_DosHilos_Runnable/src/vk/tati/HiloContador.java
@@ -5,13 +5,18 @@ package vk.tati;
 */
 
 public class HiloContador implements Runnable{
-	
+	//Como no heredamos el atributo name de Thread, definimos uno y sus get y set
+	//Es necesario porque nos han pedido controlar qué hilo va imprimiendo
+	//Hay otra forma más elegante de hacerlo, pero no lo sabía cuando subí esta versión
+	//Subo ésta comentada; la siguiente revisión será ya la definitiva
 	private String name;
 	public void setName(String n){name = n;}
 	public String getName(){return name;}
 	
+	//Definimos el constructor, por parámetro se pasará el nombre
 	public HiloContador(String n){name = n;}
 	
+	//Idéntico al run() del ejercicio PSP_UT2_DosHilos_Thread
 	public void run(){
 		System.out.println("Comienza la ejecución de " +name);
 		for (int i=2; i<201; i+=2){System.out.println(name +" imprimiendo " +i);}
