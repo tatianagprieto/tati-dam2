e9dbbd0e167eda575bcc6cf21655c7a7cacd64c0
tatianagprieto@gmail.com <tatianagprieto@gmail.com@56ce70eb-5c0e-736a-ac54-b164219d9608>
2014-11-07 00:48:57 +0000

    Segundo intento, más ajustado al enunciado, pero no rula.

--- trunk/PSP/PSP_UT2_OrdenHilos/src/vk/tati/Hilo.java
+++ trunk/PSP/PSP_UT2_OrdenHilos/src/vk/tati/Hilo.java
@@ -1,10 +1,20 @@
 package vk.tati;
 
-public class Hilo implements Runnable {
+import java.util.concurrent.Semaphore;
 
+public class Hilo implements Runnable {
+	private Semaphore sm;
+	public Hilo(Semaphore sm){this.sm = sm;}
 	@Override
 	public void run() {
+		try {
+			sm.acquire();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("Me presento; soy el Thread " +Thread.currentThread().getName());
+		sm.release();
 	}
 
 }
