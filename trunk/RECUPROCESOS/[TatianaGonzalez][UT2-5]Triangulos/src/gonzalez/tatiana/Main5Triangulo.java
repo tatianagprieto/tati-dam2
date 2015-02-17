1d6df0fc7d1373479709a4c65a714f5afb1466e0
tatianagprieto@gmail.com <tatianagprieto@gmail.com@56ce70eb-5c0e-736a-ac54-b164219d9608>
2015-02-17 02:51:21 +0000

    triangulos op

--- trunk/RECUPROCESOS/[TatianaGonzalez][UT2-5]Triangulos/src/gonzalez/tatiana/Main5Triangulo.java
+++ trunk/RECUPROCESOS/[TatianaGonzalez][UT2-5]Triangulos/src/gonzalez/tatiana/Main5Triangulo.java
@@ -1,51 +1,40 @@
 package gonzalez.tatiana;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main5Triangulo extends Thread{
-		int[] PARAMETROS = {2,3,4};
-		Recipiente recipiente = new Recipiente();	
-		
-		Comprobador comprobador = new Comprobador(recipiente);
-		/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//String[] PARAMETROS = {"2","3","4"};
 
-		//Random rng = new Random();
-		new Main5Triangulo().start();
+	public static void main(String[] args) {
+
+		Recipiente recipiente = new Recipiente();
+		Comprobador comprobador = new Comprobador(recipiente);
+		ArrayList<String>PARAMETROS = new ArrayList<String>();
+		PARAMETROS.add("2");
+		PARAMETROS.add("3");
+		PARAMETROS.add("4");
+		
+		comprobador.start();
+		
+		while (PARAMETROS.size()>0){
+			if(!recipiente.isLleno()){
+				recipiente.poner(PARAMETROS.get(0));
+				PARAMETROS.remove(0);
+			}
+		}
+		
+		try {
+			comprobador.join();
+			System.out.println(recipiente.getContenido() +" se forma triángulo");
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void run(){
 
-		
-		for (int i=0; i<PARAMETROS.length; i++){
-			while (recipiente.isLleno()){
-				try {
-					wait();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			recipiente.poner( Integer.toString(PARAMETROS[i]) );
-			notify();
-//			try {
-//				recipiente.semaforoUso.acquire();
-//				Thread.sleep(3000);
-//				recipiente.poner( Integer.toString(PARAMETROS[i]) );
-//				recipiente.semaforoUso.release();
-//				recipiente.semaforoLector.release();
-//			} catch (Exception e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-			
-		}
-		
-		comprobador.start();
 	}
 
 }
